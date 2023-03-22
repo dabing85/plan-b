a1e19de78ebf7b80bd00535783e0ba9219b35718
dabing <2261617090@qq.com>
2023-03-22 19:40:16 +0800

    8.1 附近商铺-导入店铺地理信息

--- src/main/java/com/dabing/planabc/utils/RedisConstants.java
+++ src/main/java/com/dabing/planabc/utils/RedisConstants.java
@@ -13,7 +13,7 @@ public class RedisConstants {
     public static final String SECKILL_STOCK_KEY="seckill:stock:";
     public static final String BLOG_LIKE_KEY="blog:liked:";   //笔记点赞用户set集合
     public static final String FEED_KEY="feed:";               //关注用户的blog数据 zset集合
-
+    public static final String SHOP_GEO_KEY = "shop:geo:";
 
 
 }
